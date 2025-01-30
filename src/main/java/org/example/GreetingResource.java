d7721f02387b1c534844a757007e68cf9f23893b
Reshmakachhadiya1 <152863481+Reshmakachhadiya1@users.noreply.github.com>
2025-01-30 11:43:14 +0530

    set up CI CD pipeine Test Project

---
+++ src/main/java/org/example/GreetingResource.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import io.quarkus.qute.Template;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    Template hello; // Inject the Qute template
+
+    @GET
+    @Produces(MediaType.TEXT_HTML) // Ensure correct content type
+    public String getGreeting() {
+        return hello.data("name", "Test CI CD Pipeline").render(); // Render the template
+    }
+}
